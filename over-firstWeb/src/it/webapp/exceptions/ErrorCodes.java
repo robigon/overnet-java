1c96e383a967b14a9c35bd6cf18748ab57c533f2
Roberto Giontella <r.giontella@gmail.com>
2017-12-03 14:40:34 +0100

    .

--- over-firstWeb/src/it/webapp/exceptions/ErrorCodes.java
+++ over-firstWeb/src/it/webapp/exceptions/ErrorCodes.java
@@ -17,5 +17,6 @@ package it.webapp.exceptions;
 public interface ErrorCodes {
 
 	/** Codice per errore generico */
+	/** Codice per errore generico */
 	public static final String ERROR_CODE_GENERIC = "ERR001";
 }
